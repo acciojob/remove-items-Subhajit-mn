499eba2c8111166d07b3d92b9771573fc60100ed
Subhajit Mandal <108950928+Subhajit-mn@users.noreply.github.com>
2023-10-30 16:14:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const btn = document.querySelector("input");
+btn.addEventListener("click", solve);
+function solve(event){
+				const select = document.getElementById("colorSelect");
+				let selectedValue = select.value;
+				let options = select.children;
+				for(let i=0; i<options.length; i++){
+					if(options[i].value === selectedValue){
+						options[i].remove();
+						break;
+					}
+				}
+			}
